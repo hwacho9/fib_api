729a3d3c3cdc899c1e2091f9d4dca6118e44a750
hwacho9 <csh77776@gmail.com>
2024-05-14 13:22:27 +0900

    feat: /fib get API

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 // use the express module
 const express = require("express");
 const app = express();
+
+const fibonacci = require("./fibonacci");
+const PORT = process.env.PORT || 3000;
+
+app.get('/fib', (req, res) => {
+    const n = parseInt(req.query.n);
+
+    if (isNaN(n) || n < 1) {
+        return res.status(400).json({ status: 400, message: 'Bad request.' });
+    }
+
+    const result = fibonacci(n);
+    res.json({ result });
+});
